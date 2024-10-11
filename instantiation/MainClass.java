b5cf1c6f3185fe4818aa8d3f995c7b3c96ca2a6b
tobias.weiland <tobias.weiland@7p-group.com>
2024-10-11 16:49:01 +0200

    add most basic instantiation example

---
+++ instantiation/MainClass.java
@@ -0,0 +1,12 @@
+package instantiation;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        ServiceClass serviceClass = new ServiceClass();
+
+        String message = serviceClass.returnString();
+        System.out.println(message);
+    }
+}
