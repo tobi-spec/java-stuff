c8616c0c2be4740a2d7a03c00ab82d5e7145aa4d
tobias.weiland <tobias.weiland@7p-group.com>
2024-11-15 16:19:12 +0100

    implement simple custom annotation example

---
+++ customAnnotation/simpleAnnotation/ClassToAnnotated.java
@@ -0,0 +1,13 @@
+package customAnnotation.simpleAnnotation;
+
+public class ClassToAnnotated {
+
+    @SimpleAnnotation(value = "Hello World!")
+    public void annotatedMethod() {
+        System.out.println("This is a annotated method");
+    }
+
+    public void normalMethod() {
+        System.out.println("This is a normal method");
+    }
+}
---
+++ customAnnotation/simpleAnnotation/CustomAnnotationMain.java
@@ -0,0 +1,19 @@
+package customAnnotation.simpleAnnotation;
+
+import java.lang.reflect.Method;
+
+public class CustomAnnotationMain {
+
+    public static void main(String[] args) {
+        Class<ClassToAnnotated> clazz = ClassToAnnotated.class;
+
+        for(Method method: clazz.getDeclaredMethods()) {
+            if(method.isAnnotationPresent(SimpleAnnotation.class)) {
+                SimpleAnnotation simpleAnnotation = method.getAnnotation(SimpleAnnotation.class);
+                System.out.println("Method: " + method.getName() +
+                                ", Annotation: " + method.getAnnotation(SimpleAnnotation.class) +
+                                ", Annotation Value: " + simpleAnnotation.value());
+            }
+        }
+    }
+}
