4b83281e2333d14dd8eab71be9e2b4020c7c64fe
tobias.weiland <tobias.weiland@7p-group.com>
2025-03-24 16:14:30 +0100

    delete spring-stuff basic application

--- spring-stuff/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.springstuff {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.springstuff to javafx.fxml;
-    exports org.example.springstuff;
-}
