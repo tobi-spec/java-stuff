ac6b0b22b29c79df5d9fb45424e0cf897bf53b5f
tobias.weiland <tobias.weiland@7p-group.com>
2025-04-12 18:28:50 +0200

    add service to test setup and expand mvc test with mock

--- spring-stuff/spring-test/src/main/java/org/example/springtest/HelloController.java
+++ spring-stuff/spring-test/src/main/java/org/example/springtest/HelloController.java
@@ -1,13 +1,21 @@
 package org.example.springtest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    HelloService helloService;
+
+    @Autowired
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     @GetMapping("/hello")
     public String getHello() {
-        return "Hello from Spring";
+        return helloService.greet();
     }
 }
--- spring-stuff/spring-test/src/test/java/org/example/springtest/HelloControllerMockMvcTest.java
+++ spring-stuff/spring-test/src/test/java/org/example/springtest/HelloControllerMockMvcTest.java
@@ -3,24 +3,30 @@ package org.example.springtest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(HelloController.class)
 @AutoConfigureMockMvc
 public class HelloControllerMockMvcTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private HelloService helloService;
+
     @Test
     public void testGetHello() throws Exception {
-       mockMvc.perform(get("/hello"))
+        when(helloService.greet()).thenReturn("Hello from Spring");
+        mockMvc.perform(get("/hello"))
                .andExpect(status().isOk())
                .andExpect(content().string(containsString("Hello from Spring")));
     }
