a322cef11e8009b5c53f9fc655530b34601f6987
tobias.weiland <tobias.weiland@7p-group.com>
2025-04-12 15:34:20 +0200

    add simple integration test for controller

---
+++ spring-stuff/spring-test/src/main/java/org/example/springtest/HelloController.java
@@ -0,0 +1,13 @@
+package org.example.springtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello from Spring";
+    }
+}
---
+++ spring-stuff/spring-test/src/test/java/org/example/springtest/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package org.example.springtest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerTest {
+
+    @LocalServerPort
+    public int port;
+
+    @Autowired
+    HelloController helloController;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void testGetHello() {
+        ResponseEntity<String> response = testRestTemplate.getForEntity(
+                "http://localhost:" + port + "/hello", String.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Assertions.assertEquals(response.getBody(), "Hello from Spring");
+    }
+}
