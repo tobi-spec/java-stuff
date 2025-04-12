91234cac883980de1f6939e651fe5cc5b6e362fb
tobias.weiland <tobias.weiland@7p-group.com>
2025-04-12 15:46:13 +0200

    add MockMvc test

---
+++ spring-stuff/spring-test/src/test/java/org/example/springtest/HelloControllerMockMvcTest.java
@@ -0,0 +1,27 @@
+package org.example.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerMockMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetHello() throws Exception {
+       mockMvc.perform(get("/hello"))
+               .andExpect(status().isOk())
+               .andExpect(content().string(containsString("Hello from Spring")));
+    }
+}
