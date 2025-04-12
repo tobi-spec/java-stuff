4012ffccf1bfe8b57b532ee2866c7fc83001073c
tobias.weiland <tobias.weiland@7p-group.com>
2025-04-12 14:09:21 +0200

    renaming

--- spring-stuff/spring-properties/src/main/java/org/example/springproperties/GreetingController.java
+++ spring-stuff/spring-properties/src/main/java/org/example/springproperties/GreetingController.java
@@ -1,6 +1,4 @@
 package org.example.springproperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +8,18 @@ import java.util.HashMap;
 @RestController
 public class GreetingController {
 
-    private final SingleGreetingService singleGreetingService;
+    private final SinglePropertieService singlePropertieService;
     private final MultiplePropertiesService multiplePropertiesService;
 
     @Autowired
-    public GreetingController(SingleGreetingService singleGreetingService, MultiplePropertiesService multiplePropertiesService) {
-        this.singleGreetingService = singleGreetingService;
+    public GreetingController(SinglePropertieService singlePropertieService, MultiplePropertiesService multiplePropertiesService) {
+        this.singlePropertieService = singlePropertieService;
         this.multiplePropertiesService = multiplePropertiesService;
     }
 
     @GetMapping("/greeting")
     public String getGreeting() {
-        return singleGreetingService.getGreeting();
+        return singlePropertieService.getGreeting();
     }
 
     @GetMapping("/greetings")
--- spring-stuff/spring-properties/src/main/java/org/example/springproperties/SinglePropertieService.java
+++ spring-stuff/spring-properties/src/main/java/org/example/springproperties/SinglePropertieService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SingleGreetingService {
+public class SinglePropertieService {
 
     @Value("${app.greeting}")
     private String greetingMessage;
