63eb7bced74b0f9a323c64602d65729e19c55d73
Tobias Weiland <tobias.weiland@7p-group.com>
2025-08-26 08:46:05 +0200

    add example to AccessArray

--- djl-stuff/djl-array/src/main/java/com/example/djlarray/AccessArray.java
+++ djl-stuff/djl-array/src/main/java/com/example/djlarray/AccessArray.java
@@ -1,5 +1,6 @@
 package com.example.djlarray;
 
+import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDManager;
 import ai.djl.ndarray.index.NDIndex;
 import ai.djl.ndarray.types.Shape;
@@ -17,7 +18,12 @@ public class AccessArray {
             System.out.println("third dimension: " + complexArray.get(0, 0, 0));
             System.out.println("slice" + complexArray.get(new NDIndex("0, 0:2, 0:5")));
             System.out.println("slice" + complexArray.get(new NDIndex("0:6, 0:1, 0:3")));
+
+            NDArray n1Array = manager.create(new int[]{1,2,3,4,5});
+            System.out.println(n1Array.getInt(0));
         }
+
+
     }
 
 
