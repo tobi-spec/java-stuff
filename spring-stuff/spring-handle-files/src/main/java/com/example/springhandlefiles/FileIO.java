346734654fc7aa46def4aab250a3d327d58d631c
Tobias Weiland <tobias.weiland@7p-group.com>
2025-09-01 16:32:10 +0200

    implement simple text io

---
+++ spring-stuff/spring-handle-files/src/main/java/com/example/springhandlefiles/FileIO.java
@@ -0,0 +1,21 @@
+package com.example.springhandlefiles;
+
+import java.io.*;
+
+public class FileIO {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+        writer.write("Hello, World!");
+        writer.write("\nThis is a test file.");
+        writer.close();
+
+        BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+        String line;
+        while((line = reader.readLine()) != null){
+            System.out.println(line);
+        }
+        reader.close();
+    }
+}
