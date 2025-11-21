2610a4e5dacb565d4c2d979fa5ab9f495f63f20a
Tobias Weiland <tobias.weiland@7p-group.com>
2025-11-21 11:24:12 +0100

    add attribut print outs in ReadMat example

--- opencv/src/main/java/com/example/opencv/ReadMat.java
+++ opencv/src/main/java/com/example/opencv/ReadMat.java
@@ -4,12 +4,17 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 
+import java.util.Arrays;
+
 public class ReadMat {
 
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat loadedImage = Imgcodecs.imread("spring-stuff/spring-handle-files/src/main/resources/example.jpg");
-        System.out.println(loadedImage);
+        System.out.println("Row of Matrice: " + loadedImage.row(0));
+        System.out.println("Column of Matrice: " + loadedImage.col(0));
+        System.out.println("Channels: " + loadedImage.channels());
+        System.out.println("Values of Pixel: " + Arrays.toString(loadedImage.get(0, 0)));
     }
 
 
