d590d241bef650bf60b731e7945479756ed3718d
Tobias Weiland <tobias.weiland@7p-group.com>
2026-03-20 16:07:47 +0100

    add example for connection pools

---
+++ system-design-stuff/postgres-connection-pool/src/main/java/com/example/postgresconnectionpool/PostgresConnectionPoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgresconnectionpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresConnectionPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresConnectionPoolApplication.class, args);
+    }
+
+}
---
+++ system-design-stuff/postgres-connection-pool/src/test/java/com/example/postgresconnectionpool/PostgresConnectionPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgresconnectionpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresConnectionPoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
